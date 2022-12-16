de79aac7bf56d37cd93e32ad875ecce1af4ea68c
Kavinesh8476 <kavineshtamil18@gmail.com>
2022-12-16 21:38:09 +0530

    upload program code

--- myfirst.py
+++ myfirst.py
@@ -1,33 +1,3 @@
-# Developing a Simple Webserver
-
-# AIM:
-To Display Three Programming Language
-
-# DESIGN STEPS:
-
-## Step 1:
-
-HTML content creation is done
-
-## Step 2:
-
-Design of webserver workflow
-
-## Step 3:
-
-Implementation using Python code
-
-## Step 4:
-
-
-Serving the HTML pages.
-
-## Step 5:
-
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 content ="""
@@ -51,9 +21,3 @@ class HelloHandler(BaseHTTPRequestHandler):
 server_adress =('',80)
 httpd =HTTPServer(server_adress, HelloHandler)
 httpd.serve_forever()
-```
-## OUTPUT:
-
-## RESULT:
-
-The program is executed succesfully
